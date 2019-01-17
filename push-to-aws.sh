029208e8e21cea58c7e4b6a3765a40ad4a9c9ee5
Shein Htike <shein@SheinsHckintosh.home>
2019-01-16 23:02:41 -0500

    Changed font

--- push-to-aws.sh
+++ push-to-aws.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd .. && aws s3 sync Shein-Site s3://sheinhtike.com/ --exclude "*" --include "/*.html" --exclude "*template.html" --include "writeups/*" --include "*.css" --include "images/*" --exclude "*.DS_Store"
+cd .. && aws s3 sync Shein-Site s3://sheinhtike.com/ --exclude "*" --include "/*.html" --exclude "*template.html" --include "writeups/*" --include "*.css" --include "images/*" --exclude "*.DS_Store" --include "fonts/*"
